fbc5c4b8a316c152d76e329212c06560cb37e68e
Pratikpr8 <pratikpuri.pr@gmail.com>
2024-03-04 16:19:28 +0545

    creating custom components and removing boiler plate components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
---
+++ src/components/Light.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  variant?: "green" | "yellow" | "red";
+};
+
+const Light = ({ variant = "green" }: Props) => {
+  return (
+    <div
+      style={{
+        backgroundColor: variant,
+        borderRadius: "50%",
+        width: 50,
+        height: 50,
+      }}
+    ></div>
+  );
+}; 
+
+export default Light;
+ 
